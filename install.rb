2bb562a3ca512dc1149513ef13f685ad66811438
Tim Heuett <tim.heuett@gmail.com>
2015-03-05 16:14:56 -0800

    DRY

--- install.rb
+++ install.rb
@@ -6,7 +6,7 @@ file_name = ".rubocop.yml"
 file_path = File.join(File.expand_path File.dirname(__FILE__), file_name)
 dest_path = "~/"
 
-if File.exist? File.expand_path("~/.rubocop.yml")
+if File.exist?(File.expand_path(file_name))
   puts "File '#{File.join(dest_path,file_name)}' already exists. Overwrite? (y/n): "
   if gets.chomp[0].downcase != 'y'
     puts "Operation canceled."
