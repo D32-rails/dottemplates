383cda12a85753c3475b6133e15aeef9d3c37c3d
Tim Heuett <tim.heuett@gmail.com>
2015-03-05 16:11:09 -0800

    add install script and update README instructions

---
+++ install.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+
+file_name = ".rubocop.yml"
+file_path = File.join(File.expand_path File.dirname(__FILE__), file_name)
+dest_path = "~/"
+
+if File.exist? File.expand_path("~/.rubocop.yml")
+  puts "File '#{File.join(dest_path,file_name)}' already exists. Overwrite? (y/n): "
+  if gets.chomp[0].downcase != 'y'
+    puts "Operation canceled."
+    exit
+  end
+end
+
+FileUtils.cp(file_path, File.expand_path(dest_path))
